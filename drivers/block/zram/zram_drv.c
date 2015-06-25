86c79dfce3987588147d821e90270f2bd9b79712
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2015-06-25 15:00:14 -0700

    zram: report every added and removed device
    With dynamic device creation/removal (which will be introduced later in
    the series) printing num_devices in zram_init() will not make a lot of
    sense, as well as printing the number of destroyed devices in
    destroy_devices().  Print per-device action (added/removed) in zram_add()
    and zram_remove() instead.
    
    Example:
    
    [ 3645.259652] zram: Added device: zram5
    [ 3646.152074] zram: Added device: zram6
    [ 3650.585012] zram: Removed device: zram5
    [ 3655.845584] zram: Added device: zram8
    [ 3660.975223] zram: Removed device: zram6
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1287,6 +1287,8 @@ static int zram_add(int device_id)
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
 	zram->meta = NULL;
 	zram->max_comp_streams = 1;
+
+	pr_info("Added device: %s\n", zram->disk->disk_name);
 	return 0;
 
 out_free_queue:
@@ -1300,6 +1302,7 @@ out_free_dev:
 
 static void zram_remove(struct zram *zram)
 {
+	pr_info("Removed device: %s\n", zram->disk->disk_name);
 	/*
 	 * Remove sysfs first, so no one will perform a disksize
 	 * store while we destroy the devices
@@ -1326,7 +1329,6 @@ static void destroy_devices(void)
 	idr_for_each(&zram_index_idr, &zram_remove_cb, NULL);
 	idr_destroy(&zram_index_idr);
 	unregister_blkdev(zram_major, "zram");
-	pr_info("Destroyed device(s)\n");
 }
 
 static int __init zram_init(void)
@@ -1344,9 +1346,6 @@ static int __init zram_init(void)
 		if (ret != 0)
 			goto out_error;
 	}
-
-	show_mem_notifier_register(&zram_show_mem_notifier_block);
-	pr_info("Created %u device(s) ...\n", num_devices);
 
 	return 0;
 
