1bc821915506ca4d0e405abc324443b737a87f5b
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 17:37:32 -0700

    qcacld-2.0: Fix misleading indentation warning in limSendDelBAInd
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -5547,7 +5547,7 @@ tSirRetStatus limSendDelBAInd( tpAniSirGlobal pMac,
     else
       return eSIR_SUCCESS;
 
-      returnAfterError:
+returnAfterError:
 
       // Release buffer, if allocated
       if( NULL != pDelBAIndBuffer )
