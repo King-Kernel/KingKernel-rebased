899ca1149035357c4c67d77e73ca381e60fa1ccd
Nathan Chancellor <natechancellor@gmail.com>
2018-05-10 18:02:54 -0700

    mdss: hdmi: Fix header guard
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/uapi/video/msm_hdmi_hdcp_mgr.h
+++ include/uapi/video/msm_hdmi_hdcp_mgr.h
@@ -1,5 +1,5 @@
-#ifndef _UAPI__HDMI_HDCP_MGR_H
-#define _UAPI__HDMI_HDCP_MGR_H
+#ifndef _UAPI__MSM_HDMI_HDCP_MGR_H
+#define _UAPI__MSM_HDMI_HDCP_MGR_H
 
 enum DS_TYPE {  /* type of downstream device */
 	DS_UNKNOWN,
