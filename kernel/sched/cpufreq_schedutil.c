56bd12fffafe67ab32951333c268722b61daf7fe
Danny Lin <danny@kdrag0n.dev>
2019-09-09 16:47:51 -0400

    BACKPORT: cpufreq: schedutil: Expose default rate-limits as config options
    This allows us to tune the default rate-limits without constantly
    changing schedutil's code.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    [kingbri: backported to 3.18 and use kerneltoast's code for forcing tuneables]
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -724,8 +724,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
         /* Hard-code some sane rate-limit values */
-        tunables->up_rate_limit_us = 10000;
-        tunables->down_rate_limit_us = 20000;
+        tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
+        tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
 
 	tunables->iowait_boost_enable = policy->iowait_boost_enable;
 
