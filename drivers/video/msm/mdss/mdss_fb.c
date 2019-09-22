2c838e99d35e1f3e235ce5cb24d2ebdda32d3520
kingbri <bdashore3@gmail.com>
2019-09-22 12:03:07 -0400

    mdss: set backlight dimmer as true
    It saves battery and can be disabled if the user wants to
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -85,7 +85,7 @@
 			out = (12*v*v+1393*v+3060)/4465;\
 			} while (0)
 
-bool backlight_dimmer = false;
+bool backlight_dimmer = true;
 module_param(backlight_dimmer, bool, 0755);
 
 static struct fb_info *fbi_list[MAX_FBI_LIST];
