af6b59533ec427a391b78d81e999a5b7c651ac1e
kingbri <bdashore3@gmail.com>
2019-09-27 09:08:21 -0400

    fs: f2fs: fix syntax error
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2600,8 +2600,7 @@ static int sanity_check_raw_super(struct f2fs_sb_info *sbi,
 	}
 
 	if (le32_to_cpu(raw_super->segment_count) > F2FS_MAX_SEGMENT) {
-		f2fs_msg(sb, KERN_INFO,
-			"Invalid segment count (%u)",
+		f2fs_info(sbi, "Invalid segment count (%u)",
 			le32_to_cpu(raw_super->segment_count));
 		return 1;
 	}
